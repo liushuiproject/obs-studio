30582072fafe6bad9e9b44a17e7e817a73ed0c04
Exeldro <exelsor@hotmail.com>
2020-04-22 14:01:18 +0200

    frontend-tools: Add edit script button

--- UI/frontend-plugins/frontend-tools/scripts.cpp
+++ UI/frontend-plugins/frontend-tools/scripts.cpp
@@ -13,6 +13,7 @@
 #include <QDialogButtonBox>
 #include <QResizeEvent>
 #include <QAction>
+#include <QDesktopServices>
 
 #include <obs.hpp>
 #include <obs-module.h>
@@ -357,6 +358,16 @@ void ScriptsTool::on_reloadScripts_clicked()
 				     .constData());
 
 	on_scripts_currentRowChanged(ui->scripts->currentRow());
+}
+
+void ScriptsTool::on_editScript_clicked()
+{
+	int row = ui->scripts->currentRow();
+	if (row == -1)
+		return;
+	QUrl url = QUrl::fromLocalFile(
+		ui->scripts->item(row)->data(Qt::UserRole).toString());
+	QDesktopServices::openUrl(url);
 }
 
 void ScriptsTool::on_scriptLog_clicked()
